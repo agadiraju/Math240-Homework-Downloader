54dca1e474c18701620003ceffad96b312cf09e8
Clara Wu <Clara@Solve-Medias-iMac.local>
2012-10-25 15:08:41 -0400

    Updated Math240 script to include lectures

--- math240.py
+++ math240.py
@@ -15,8 +15,20 @@ for link in links:
    if "homework" in str(link):
       heads = "http://www.math.upenn.edu"
       mini_link = str(link['href'])
-      if "/" in link['href']:
+      if "/" in mini_link:
          result = re.findall('[^\/]+$', mini_link)
          urllib.urlretrieve(heads + mini_link, result[0])
       else:
          urllib.urlretrieve(heads + "/~moose/" + mini_link, mini_link)
+
+request = urllib2.urlopen("http://www.math.upenn.edu/~ryblair/Math240F12/index.html")
+page2 = request.read()
+second = BeautifulSoup(page2)
+
+links2 = second.findAll('a', href=True)
+for link in links2:
+   if "Slides" in str(link):
+      heads = "http://www.math.upenn.edu/~ryblair/Math240F12/"
+      mini_link=str(link['href'])
+      result = re.findall('[^\/]+$', mini_link)
+      urllib.urlretrieve(heads + mini_link, result[0])
